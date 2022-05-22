97a54fc2ff25a710643bf26b5018c49aaef857a2
1nv1 <nelson.lombardo@gmail.com>
2022-05-22 17:01:34 -0300

    Minor changes

--- kvdb.js
+++ kvdb.js
@@ -5,10 +5,10 @@ class kvdb {
     // UUID generator
     this._uuid = function() {
       var dt = new Date().getTime();
-      var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = (dt + Math.random()*16)%16 | 0;
+      var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+        var r = (dt + Math.random() * 16) % 16 | 0;
         dt = Math.floor(dt/16);
-        return (c=='x' ? r : (r & 0x3 | 0x8)).toString(16);
+        return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
       });
       return uuid;
     };
